09c7b3e2426e3deca14414e1cde384aabb713bde
Jon Sneyers <jon.sneyers@cs.kuleuven.be>
2015-09-14 16:28:38 +0200

    added license info, check unreadable input

--- flif.cpp
+++ flif.cpp
@@ -1,3 +1,28 @@
+/*  FLIF - Free Lossless Image Format
+ Copyright (C) 2010-2015  Jon Sneyers & Pieter Wuille
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ Parts of this code are based on code from the FFMPEG project, in
+ particular parts:
+ - ffv1.c - Copyright (c) 2003 Michael Niedermayer <michaelni@gmx.at>
+ - common.h - copyright (c) 2006 Michael Niedermayer <michaelni@gmx.at>
+ - rangecoder.c - Copyright (c) 2004 Michael Niedermayer <michaelni@gmx.at>
+*/
+
 #include <string>
 #include <string.h>
 
@@ -697,6 +722,7 @@ bool encode(const char* filename, Image &image, std::vector<std::string> transDe
         } else {
             if (tcount++ > 0) printf(", ");
             printf("%s", transDesc[i].c_str());
+            fflush(stdout);
             rac.write(true);
             write_name(rac, transDesc[i]);
             trans->save(rangesList.back(), rac);
@@ -1018,11 +1044,14 @@ int main(int argc, char **argv)
   printf("    _____   ___     ___   _____ \n");
   printf("   (  ___) (   )   (   ) (  ___)       FLIF 0.1   [September 2015]\n");
   printf("    ) __)   ) (__   ) (   ) __)        by Jon Sneyers & Pieter Wuille\n");
-  printf("   (__)    (_____) (___) (__)          (c) 2010-2015;  GNU GPL v3.\n");
+  printf("   (__)    (_____) (___) (__)          (c) 2010-2015;  GNU GPL v3+\n");
   printf("----------------------------------------------------------------------\n");
 
   if (mode == 0) {
-        image.load(argv[0]);
+        if (!image.load(argv[0])) {
+           fprintf(stderr,"Could not read input file.\n");
+           return 1;
+        };
         uint64_t nb_pixels = (uint64_t)image.rows() * image.cols();
         std::vector<std::string> desc;
         desc.push_back("YIQ");  // convert RGB(A) to YIQ(A)
