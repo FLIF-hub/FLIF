966311748dc67da54cbfa00690d744346ac0e119
hrj <harshad.rj@gmail.com>
2015-10-07 14:13:34 +0530

    test with different command line parameters

--- tools/test-roundtrip.sh
+++ tools/test-roundtrip.sh
@@ -6,6 +6,17 @@ IN=$1
 OUTF=$2
 OUTP=$3
 
-./flif "${IN}" "${OUTF}"
-./flif -d ${OUTF} ${OUTP}
-test "`compare -metric AE ${IN} ${OUTP} null 2>&1`" = "0"
+runtest() {
+  local encArgs=$1
+  local decArgs=$2
+
+  ./flif $encArgs "${IN}" "${OUTF}"
+  ./flif -d $decArgs ${OUTF} ${OUTP}
+  test "`compare -metric AE ${IN} ${OUTP} null 2>&1`" = "0"
+}
+
+runtest 
+
+runtest -i
+
+runtest -n
