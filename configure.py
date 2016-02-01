55d07d20ec5694507c08f286770203e174b38831
Orson Peters <orsonpeters@gmail.com>
2016-02-01 11:00:35 +0100

    Added first time no ninja info.

--- configure.py
+++ configure.py
@@ -81,7 +81,47 @@ download https://bootstrap.pypa.io/get-pip.py and run it."""
     print(msg.format(os.path.basename(sys.executable)))
     sys.exit(1)
 
+
+# Check if we're likely running for the first time.
+first_time = not os.path.isfile("build.ninja")
+
 args = docopt(__doc__)
 with open("build.ninja", "w") as ninja_file:
     n = ninja_syntax.Writer(ninja_file)
     write_ninja(n, args)
+
+# Check if we have ninja in path.
+path = os.environ.get("PATH", os.defpath).split(os.pathsep)
+if sys.platform == "win32":
+    path.insert(0, os.curdir)
+    pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+    files = ["ninja" + ext for ext in pathext]
+else:
+    files = ["ninja"]
+
+is_exe = lambda p: os.path.exists(p) and os.access(p, os.F_OK | os.X_OK) and not os.path.isdir(p)
+has_ninja = any(is_exe(os.path.join(d, f)) for d in path for f in files)
+
+if first_time and not has_ninja:
+    msg = """It appears you're running configure.py for the first time, but do not have
+ninja in your path. On Windows we recommend simply downloading the binary:
+
+    https://github.com/ninja-build/ninja/releases/download/v1.6.0/ninja-win.zip
+
+Extract anywhere in your path, or even inside this directory.
+
+On linux it's easiest to compile from source:
+
+    wget https://github.com/ninja-build/ninja/archive/v1.6.0.tar.gz
+    tar xf v1.6.0.tar.gz && rm v1.6.0.tar.gz && cd ninja-1.6.0
+    {} configure.py --bootstrap
+    sudo cp ninja /usr/local/bin
+
+This should only take half a minute or so."""
+    print(msg.format(os.path.basename(sys.executable)))
+
+
+
+
+
+
