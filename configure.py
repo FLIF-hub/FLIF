b4dd89ec5a401258ce65b05454e1783e099e3db0
Orson Peters <orsonpeters@gmail.com>
2016-02-01 10:30:05 +0100

    Updated configure.py.

--- configure.py
+++ configure.py
@@ -1,7 +1,7 @@
-import ninja_syntax
-from docopt import docopt
-import os.path
+import os
+import sys
 import fnmatch
+
 
 __doc__ = """Configurator for FLIF build.
 
@@ -15,56 +15,73 @@ Options:
   -h, --help     Show this screen.
 """
 
-arguments = docopt(__doc__)
 
-ninja_file = open("build.ninja", "w")
-n = ninja_syntax.Writer(ninja_file)
+def write_ninja(n, args):
+    ########################
+    # Generic build rules. #
+    ########################
+    n.variable("cc", "gcc")
+    n.variable("cxx", "g++")
 
-########################
-# Generic build rules. #
-########################
-n.variable("cc", "gcc")
-n.variable("cxx", "g++")
+    if args["--debug"]:
+        n.variable("dbgflags", "-ggdb")
+        debug_flag = ""
+    else:
+        n.variable("dbgflags", "")
+        debug_flag = " -DNDEBUG"
 
-if arguments["--debug"]:
-    n.variable("dbgflags", "-ggdb")
-    debug_flag = ""
-else:
-    n.variable("dbgflags", "")
-    debug_flag = " -DNDEBUG"
+    native_flag = " -march=native" if args["--native"] else ""
+    n.variable("optflags", "-O2 -ftree-vectorize" + native_flag + debug_flag)
 
-native_flag = " -march=native" if arguments["--native"] else ""
-n.variable("optflags", "-O2 -ftree-vectorize" + native_flag + debug_flag)
+    defines = [" -D" + d for d in args["-D"]]
 
-defines = [" -D" + d for d in arguments["-D"]]
+    cxxflags = "-std=c++11 -Wall -pedantic `pkg-config --cflags zlib libpng`"
+    n.variable("cxxflags", cxxflags + "".join(defines))
+    n.variable("cxxlinkflags", "`pkg-config --libs libpng`")
 
-cxxflags = "-std=c++11 -Wall -pedantic `pkg-config --cflags zlib libpng`"
-n.variable("cxxflags", cxxflags + "".join(defines))
-n.variable("cxxlinkflags", "`pkg-config --libs libpng`")
+    n.rule("cxx",
+           "$cxx $xtype -MMD -MF $out.d $optflags $dbgflags $cxxflags -c $in -o $out",
+           depfile="$out.d")
 
-n.rule("cxx",
-       "$cxx $xtype -MMD -MF $out.d $optflags $dbgflags $cxxflags -c $in -o $out",
-       depfile="$out.d")
+    n.rule("cxxlink", "$cxx $optflags $dbgflags $in $cxxlinkflags -o $out")
 
-n.rule("cxxlink", "$cxx $optflags $dbgflags $in $cxxlinkflags -o $out")
+    ###################
+    # Build commands. #
+    ###################
 
-###################
-# Build commands. #
-###################
+    # Precompiled header.
+    n.build("src/precompile.h.gch", "cxx", "src/precompile.h", variables={"xtype": "-x c++-header"})
 
-# Precompiled header.
-n.build("src/precompile.h.gch", "cxx", "src/precompile.h", variables={"xtype": "-x c++-header"})
+    objects = []
+    for root, dirnames, filenames in os.walk("src"):
+        for filename in fnmatch.filter(filenames, "*.cpp"):
+            if "flif-interface" in filename:
+                continue
+            src = os.path.join(root, filename)
+            obj = os.path.join("obj", os.path.splitext(src)[0] + ".o")
+            n.build(obj, "cxx", src, "src/precompile.h.gch")
+            objects.append(obj)
 
-objects = []
-for root, dirnames, filenames in os.walk("src"):
-    for filename in fnmatch.filter(filenames, "*.cpp"):
-        if "flif-interface" in filename: continue
-        src = os.path.join(root, filename)
-        obj = os.path.join("obj", os.path.splitext(src)[0] + ".o")
-        n.build(obj, "cxx", src, "src/precompile.h.gch")
-        objects.append(obj)
+    exe_ext = ".exe" if os.name == "nt" else ""
+    n.build("src/flif" + exe_ext, "cxxlink", objects)
+    n.default("src/flif" + exe_ext)
 
-n.build("src/flif", "cxxlink", objects)
-n.default("src/flif")
 
-ninja_file.close()
+try:
+    from docopt import docopt
+    import ninja_syntax
+except ImportError:
+    msg = """You are missing one or more dependencies, install using:
+
+    {} -m pip install --user docopt ninja-syntax
+
+If you are using an old version of Python, and don't have pip,
+download https://bootstrap.pypa.io/get-pip.py and run it."""
+
+    print(msg.format(os.path.basename(sys.executable)))
+    sys.exit(1)
+
+args = docopt(__doc__)
+with open("build.ninja", "w") as ninja_file:
+    n = ninja_syntax.Writer(ninja_file)
+    write_ninja(n, args)
