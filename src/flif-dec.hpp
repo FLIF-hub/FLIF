11d73aefc7521b7c837e047242f8f69983053d88
fherzog2 <fherzog2@users.noreply.github.com>
2016-09-06 21:03:09 +0200

    #296 Implement API for creating and accessing FLIF_INFO objects.
    This enables fast access to the attributes of a FLIF image without having to decode the whole image.

--- src/flif-dec.hpp
+++ src/flif-dec.hpp
@@ -1,9 +1,24 @@
 #pragma once
 
+struct FLIF_INFO
+{
+    FLIF_INFO();
+
+    uint32_t width;
+    uint32_t height;
+    uint8_t channels;
+    uint8_t bit_depth;
+    size_t num_images;
+};
+
+/*!
+* @param[out] info An info struct to fill. If this is not a null pointer, the decoding will exit after reading the file header.
+*/
+
 template <typename IO>
-bool flif_decode(IO& io, Images &images, int quality, int scale, uint32_t (*callback)(int32_t,int64_t), int, Images &partial_images, int rw, int rh, int crc_check, bool fit, metadata_options &md);
+bool flif_decode(IO& io, Images &images, int quality, int scale, uint32_t (*callback)(int32_t,int64_t), int, Images &partial_images, int rw, int rh, int crc_check, bool fit, metadata_options &md, FLIF_INFO* info);
 
 template <typename IO>
 bool flif_decode(IO& io, Images &images, int quality, int scale, int rw, int rh, int crc_check, bool fit, metadata_options &md) {
-    return flif_decode(io, images, quality, scale, NULL, 0, images, rw, rh, crc_check, fit, md);
+    return flif_decode(io, images, quality, scale, NULL, 0, images, rw, rh, crc_check, fit, md, 0);
 }
