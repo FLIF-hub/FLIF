024b039f1f6181f03b86a35ead02cdc88332f2fb
Jon Sneyers <jon.sneyers@gmail.com>
2016-09-22 14:48:49 +0200

    FLIF 0.2 release

--- src/flif.cpp
+++ src/flif.cpp
@@ -212,7 +212,7 @@ bool file_is_flif(const char * filename){
 
 void show_banner() {
     v_printf(3,"  ____ _(_)____\n");
-    v_printf(3," (___ | | | ___)   ");v_printf(2,"FLIF (Free Lossless Image Format) 0.2.0rc19 [26 Aug 2016]\n");
+    v_printf(3," (___ | | | ___)   ");v_printf(2,"FLIF (Free Lossless Image Format) 0.2.0 [22 Sept 2016]\n");
     v_printf(3,"  (__ | |_| __)    ");v_printf(3,"Copyright (C) 2016 Jon Sneyers and Pieter Wuille\n");
     v_printf(3,"    (_|___|_)      ");
 #ifdef HAS_ENCODER
