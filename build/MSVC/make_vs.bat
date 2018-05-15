07390d2a5318a7662e67837b8405308b3f49eaea
Sylvain Debaudringhien <sylvain.debaudringhien@supoptique.org>
2018-05-15 11:58:15 +0200

    Sylvain Debaudringhien: Updated Visual Studio build script, to get it to work using VS2017 command prompt. Added .obj files cleanup.

--- build/MSVC/make_vs.bat
+++ build/MSVC/make_vs.bat
@@ -1,16 +1,14 @@
-cd /d %~dp0..\..\
-
 echo release
-cl /nologo /DFLIF_USE_STB_IMAGE /Feflif.exe flif.cpp build\MSVC\getopt\*.c* -I build\MSVC\getopt\ /DSTATIC_GETOPT flif-dec.cpp flif-enc.cpp common.cpp io.cpp maniac\*.c* transform\*.cpp image\*.c*  /WX /EHsc /Ox /Oy /MT /DNDEBUG
+cl /nologo /DFLIF_USE_STB_IMAGE /Feflif.exe -I getopt\ /DSTATIC_GETOPT ..\..\src\flif.cpp ..\..\src\flif-dec.cpp ..\..\src\flif-enc.cpp ..\..\src\common.cpp ..\..\src\io.cpp ..\..\src\maniac\*.c* ..\..\src\transform\*.cpp ..\..\src\image\*.c* getopt/getopt.c ..\..\extern\lodepng.cpp /WX /EHsc /Ox /Oy /MT /DNDEBUG
 
 echo debug
-cl /nologo /DFLIF_USE_STB_IMAGE /Feflif-d.exe flif.cpp build\MSVC\getopt\*.c* -I build\MSVC\getopt\ /DSTATIC_GETOPT flif-dec.cpp flif-enc.cpp common.cpp io.cpp maniac\*.c* transform\*.cpp image\*.c*  /WX /EHsc /Zi /Oy- /MDd /DDEBUG
+cl /nologo /DFLIF_USE_STB_IMAGE /Feflif-d.exe -I getopt\ /DSTATIC_GETOPT ..\..\src\flif.cpp ..\..\src\flif-dec.cpp ..\..\src\flif-enc.cpp ..\..\src\common.cpp ..\..\src\io.cpp ..\..\src\maniac\*.c* ..\..\src\transform\*.cpp ..\..\src\image\*.c* getopt/getopt.c ..\..\extern\lodepng.cpp /WX /EHsc /Zi /Oy- /MDd /DDEBUG
+
+del *.obj
 
 echo test
 if exist output.flif del output.flif
-flif tools/kodim01.png output.flif
+flif ..\..\tools/kodim01.png output.flif
 if exist output.png  del output.png
 flif output.flif output.png
 start output.png
-
-
